224bd9561763736c19b4eb0ca8a403035365f7af
Hugo Gonzalez <hugo11gonzalez@gmail.com>
2023-02-27 14:25:14 -0300

    3er commit terminado js upcoming

--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -26,7 +26,7 @@ function mostrarEventosfuturos(eventosfuturos) {
         eventosHtml += `<div class="col-12 col-sm-10 col-md-9 col-lg-4 col-xl-3 p-0 d-flex align-self-lg-stretch justify-content-center">
         
           <div class="card d-flex flex-column w-100">
-            <img src= ${evento.image} class="card-img-top img-thumbnail" alt="...">
+            <img src= ${evento.image} class="card-img-top img-thumbnail img-fluid" alt="...">
             <div class="card-body">
               <h5 class="card-title">${evento.name}</h5>
               <p class="card-text">${evento.description}</p>
